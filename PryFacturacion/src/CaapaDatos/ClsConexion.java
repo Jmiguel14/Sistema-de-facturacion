41d80aa17b7a262ffe319e3d56cabdcb97f7dc45
Jmiguel14 <56648593+Jmiguel14@users.noreply.github.com>
2019-10-27 11:31:19 -0500

    Conexion con BDD realizada

--- PryFacturacion/src/CaapaDatos/ClsConexion.java
+++ PryFacturacion/src/CaapaDatos/ClsConexion.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pryfacturacion;
+package CaapaDatos;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
